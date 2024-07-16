5d78cd6165b8b2248f997fcb90397b6749ed76d7
arabomeivan <arabomeivan@protonmail.com>
2024-07-16 03:16:56 +0100

    added baseURL for cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: 'https://crain-front-end.vercel.app/',
     browser: 'edge',
     setupNodeEvents(on, config) {
       // implement node event listeners here
